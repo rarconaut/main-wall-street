5fc0e21bc0593ef547f953b930a00e9d9fe7d9e8
rarconaut <68246130+rarconaut@users.noreply.github.com>
2021-01-08 20:49:16 -0800

    ...still attempt 1

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 #################################################
+# Main-Wall-Street UCSD Data Science & Viz Bootcamp
+# Project 2 Group 6 Bretton, Brice, Gunjan, Jeremy, Rawaf
+#################################################
+
+
+#################################################
 # Dependencies (from Lesson 10 Activity 10-Ins_Flask_with_ORM)
 #################################################
 from flask import make_response, redirect, render_template, request, url_for
 from flask import current_app as app
-# from datetime import datetime as dt
-# import numpy as np
 
 import sqlalchemy
 # from sqlalchemy.ext.automap import automap_base
@@ -31,13 +35,12 @@ from flask import Flask, jsonify
 #################################################
 # Flask Setup
 #################################################
-# from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost/project_2_corp_capital"
-# f"postgresql://{username}:{password}@localhost/{database}"
+
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -67,21 +70,6 @@ class Data(db.Model):
 
 
 
-# """Example table"""
-# class User(db.Model):
-
-# __tablename__ = "flasksqlalchemy-tutorial-users"
-# id = db.Column(db.Integer, primary_key=True)
-# username = db.Column(db.String(64), index=False, unique=True, nullable=False)
-# email = db.Column(db.String(80), index=True, unique=True, nullable=False)
-# created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-# bio = db.Column(db.Text, index=False, unique=False, nullable=True)
-# admin = db.Column(db.Boolean, index=False, unique=False, nullable=False)
-
-# def __repr__(self):
-#     return "<User {}>".format(self.username)
-
-
 #################################################
 # Flask Routes
 #################################################
@@ -94,16 +82,13 @@ def home():
 
 
 # Create a different route for each element's API (built from querying our database)
-# - these will be the source URLs for our visualizations' .js files
+# - these will be the source json URLs for our visualizations' .js files
 @app.route("/api")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
         f"/api/combined-data<br/>"
-        # f"/api/map<br/>"
-        # f"/api/hex<br/>"
-        # f"/api/graph"
     )
 
 @app.route("/api/combined-data")
@@ -139,26 +124,7 @@ def combinedData():
     net = [result[11] for result in results]
     cap = [result[12] for result in results]
     mhi = [result[13] for result in results]
-    # lat = [result[x] for result in results]
-
-    # combined_data = [{
-        # "ticker": ticker,
-        # "exchange": exchange,
-        # "sector": sector,
-        # "industry": industry,
-        # "state": state,
-        # "city": city,
-        # "lat": lat,
-        # "lon": lon,
-        # "report_year": year,
-        # "revenue": revenue,
-        # "income_tax": tax,
-        # "net_income": net,
-        # "market_cap": cap,
-        # "median_hh_income": mhi
-    # }]
-
-      # return jsonify(combined_data)
+  
 
     # Create a dictionary for each company (from the row data) and append to a list of all_passengers
     all_data = []
@@ -197,78 +163,6 @@ def combinedData():
 
     return jsonify(all_data)
 
-  
-
-
-# @app.route("/api/map", methods=["GET"])
-# def mapAPI():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of all a Table and its entries"""
-#     # Query all entries
-#     results = session.query(Table.column).all()
-
-#     session.close()
-
-#     # Convert list of tuples into normal list
-#     all_entries = list(np.ravel(results))
-
-#     return jsonify(all_entries)
-
-
-# @app.route("/api/hex", methods=["GET"])
-# def hexAPI():
-#     # Or for more complex queries, create and append a new custom list
-
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Example: Return a list of passenger data including the name, age, and sex"""
-#     # # Query all passengers
-#     # results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-#     session.close()
-
-#     # # Create a dictionary for each passenger (from the row data) and append to a list of all_passengers
-#     # all_passengers = []
-#     # for name, age, sex in results:
-#     #     passenger_dict = {}
-#     #     passenger_dict["name"] = name
-#     #     passenger_dict["age"] = age
-#     #     passenger_dict["sex"] = sex
-#     #     all_passengers.append(passenger_dict)
-
-#     # return jsonify(all_passengers)
-
-
-# @app.route("/api/graph", methods=["GET"])
-# def graphAPI():
-
-
-# @app.route("/", methods=["GET"])
-# def user_records():
-#     # """Create a user via query string parameters."""
-#     username = request.args.get("user")
-#     email = request.args.get("email")
-#     if username and email:
-#         existing_user = User.query.filter(
-#             User.username == username or User.email == email
-#         ).first()
-#         if existing_user:
-#             return make_response(f"{username} ({email}) already created!")
-#         new_user = User(
-#             username=username,
-#             email=email,
-#             created=dt.now(),
-#             bio="In West Philadelphia born and raised, \
-#             on the playground is where I spent most of my days",
-#             admin=False,
-#         )  # Create an instance of the User class
-#         db.session.add(new_user)  # Adds new User record to database
-#         db.session.commit()  # Commits all changes
-#         redirect(url_for("user_records"))
-#     return render_template("users.jinja2", users=User.query.all(), title="Show Users")
 
 if __name__ == '__main__':
     app.run(debug=True)
